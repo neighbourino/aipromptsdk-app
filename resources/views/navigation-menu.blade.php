de4448f1b99a86e6ed847227fc6a6ac871c20751
Janus Helkjær <jh@mindmill.dk>
2024-03-15 12:27:14 +0100

    INITIAL COMMIT

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-mark class="block h-9 w-auto" />
+                        {{-- <x-application-mark class="block h-9 w-auto" /> --}}
+                        <img src="{{ asset('assets/images/logo.png') }}" alt="" class="block h-12 w-auto">
                     </a>
                 </div>
 
@@ -19,6 +20,8 @@
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
+
+                @auth
                 <!-- Teams Dropdown -->
                 @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
                     <div class="ms-3 relative">
@@ -70,11 +73,16 @@
                         </x-dropdown>
                     </div>
                 @endif
+                
+                @endauth
 
                 <!-- Settings Dropdown -->
                 <div class="ms-3 relative">
                     <x-dropdown align="right" width="48">
+
                         <x-slot name="trigger">
+
+                            @auth
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                                 <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
                                     <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
@@ -90,9 +98,24 @@
                                     </button>
                                 </span>
                             @endif
+                            @endauth
+
+                            @guest
+                                 <span class="inline-flex rounded-md">
+                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                        Log Ind
+
+                                        <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+                                    </button>
+                                </span>
+                            @endguest
                         </x-slot>
 
                         <x-slot name="content">
+
+                            @auth
                             <!-- Account Management -->
                             <div class="block px-4 py-2 text-xs text-gray-400">
                                 {{ __('Manage Account') }}
@@ -119,6 +142,7 @@
                                     {{ __('Log Out') }}
                                 </x-dropdown-link>
                             </form>
+                            @endauth
                         </x-slot>
                     </x-dropdown>
                 </div>
@@ -144,6 +168,7 @@
             </x-responsive-nav-link>
         </div>
 
+        @auth
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="flex items-center px-4">
@@ -215,5 +240,7 @@
                 @endif
             </div>
         </div>
+
+        @endauth
     </div>
 </nav>
